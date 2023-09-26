b3b5d811766dc90ebc5bc90a6ec85400d23f2616
k_czech <kamczeh@gmail.com>
2023-09-26 22:32:55 +0200

    feat:improved counter

--- src/components/atoms/Counter.tsx
+++ src/components/atoms/Counter.tsx
@@ -5,5 +5,15 @@ import { useState } from "react";
 export const Counter = () => {
 	const [count, setCount] = useState(0);
 
-	return <button onClick={() => setCount((count) => count + 1)}>{count}</button>;
+	return (
+		<>
+			<button onClick={() => setCount((count) => count - 1)} className="mr-2 border px-3 py-1">
+				-
+			</button>
+			<span>{count}</span>
+			<button onClick={() => setCount((count) => count + 1)} className="ml-2 border px-3 py-1">
+				+
+			</button>
+		</>
+	);
 };
